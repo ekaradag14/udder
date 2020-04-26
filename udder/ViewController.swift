26b8f7a68ba2ebdd878ee76e05bc636eceecae29
Erencan <ekaradag14@ku.edu.tr>
2020-04-26 14:57:27 +0300

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Erencan Karadağ on 26.04.2020.
+//  Copyright © 2020 Erencan Karadağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
